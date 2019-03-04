d73f74978f53b1ee6a178ea59a72f8a101bfcfc4
Eric Kitaif <eric.kitaif@gmail.com>
2019-03-04 06:05:38 -0800

    Added validateEmail method and new hook

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,4 +1,4 @@
-import { Schema, model, Model, Document } from 'mongoose';
+import { Schema, model, Model, Document, Query } from 'mongoose';
 import * as bcrypt from 'bcrypt';
 import { User } from '../../graphql.classes';
 
@@ -17,6 +17,10 @@ export interface UserDocument extends User, Document {
   ): void;
 }
 
+export interface IUserModel extends Model<UserDocument> {
+  validateEmail(email: string): boolean;
+}
+
 export const PasswordResetSchema: Schema = new Schema({
   token: { type: String, required: true },
   expiration: { type: Date, required: true },
@@ -28,6 +32,7 @@ export const UserSchema: Schema = new Schema(
       type: String,
       unique: true,
       required: true,
+      validate: { validator: validateEmail },
     },
     password: {
       type: String,
@@ -45,12 +50,10 @@ export const UserSchema: Schema = new Schema(
     lowercaseUsername: {
       type: String,
       unique: true,
-      required: true,
     },
     lowercaseEmail: {
       type: String,
       unique: true,
-      required: true,
     },
     passwordReset: PasswordResetSchema,
   },
@@ -59,10 +62,18 @@ export const UserSchema: Schema = new Schema(
   },
 );
 
+function validateEmail(email: string) {
+  // tslint:disable-next-line:max-line-length
+  const expression = /^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return expression.test(email);
+}
+
 // NOTE: Arrow functions are not used here as we do not want to use lexical scope for 'this'
 UserSchema.pre<UserDocument>('save', function(next) {
   const user = this;
 
+  user.lowercaseUsername = user.username.toLowerCase();
+  user.lowercaseEmail = user.email.toLowerCase();
   // Make sure not to rehash the password if it is already hashed
   if (!user.isModified('password')) {
     return next();
@@ -84,6 +95,43 @@ UserSchema.pre<UserDocument>('save', function(next) {
   });
 });
 
+UserSchema.pre<Query<UserDocument>>('findOneAndUpdate', function(next) {
+  const updateFields = this.getUpdate();
+
+  if (updateFields.username) {
+    this.update(
+      {},
+      { $set: { lowercaseUsername: updateFields.username.toLowerCase() } },
+    );
+  }
+
+  if (updateFields.email) {
+    this.update(
+      {},
+      { $set: { lowercaseEmail: updateFields.email.toLowerCase() } },
+    );
+  }
+
+  // Generate a salt and use it to hash the user's password
+  if (updateFields.password) {
+    bcrypt.genSalt(10, (genSaltError, salt) => {
+      if (genSaltError) {
+        return next(genSaltError);
+      }
+
+      bcrypt.hash(updateFields.password, salt, (err, hash) => {
+        if (err) {
+          return next(err);
+        }
+        updateFields.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 // TODO: Redo the callback to match the bcrypt or make it nicer
 UserSchema.methods.checkPassword = function(
   password: string,
@@ -99,7 +147,11 @@ UserSchema.methods.checkPassword = function(
   });
 };
 
-export const UserModel: Model<UserDocument> = model<UserDocument>(
+UserSchema.statics.validateEmail = function(email: string): boolean {
+  return validateEmail(email);
+};
+
+export const UserModel: IUserModel = model<UserDocument, IUserModel>(
   'User',
   UserSchema,
 );
