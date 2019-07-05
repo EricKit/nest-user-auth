9b55bd22eb1d47396ece318bc39df1b25ad36f33
Eric Kitaif <eric.kitaif@gmail.com>
2019-07-05 13:58:41 -0700

    Fixed bug, would not serialize id.

--- src/scalars/object-id.scalar.ts
+++ src/scalars/object-id.scalar.ts
@@ -11,7 +11,7 @@ export class ObjectIdScalar {
   }
 
   serialize(value: Types.ObjectId) {
-    return value.toHexString; // value sent to the client
+    return value.toHexString(); // value sent to the client
   }
 
   parseLiteral(ast: ASTNode) {
